c744971665fcab9f10380e93fa8c4eb1db03ee3b
Ayub Ali <pltsummary@gmail.com>
2025-02-24 16:59:06 -0700

    server set up

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,13 @@ import { PORT } from "./config.js";
 
 const app = express();
 
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("MERN stack applications");
+});
+
 app.listen(PORT, () => {
   console.log(`App is listen to port: ${PORT}`);
 });
+
+// HTTP Routing
